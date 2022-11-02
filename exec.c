dfd91b4178d529ff0b5f309bba591242e5c0ed30
Fatih Sarioglu <fsariogl@z2r12p4.42lyon.fr>
2022-11-02 19:26:42 +0100

    Pipe a moitiee bon

--- exec.c
+++ exec.c
@@ -6,7 +6,7 @@
 /*   By: fsariogl <fsariogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 14:13:18 by fsariogl          #+#    #+#             */
-/*   Updated: 2022/11/02 17:12:46 by fsariogl         ###   ########.fr       */
+/*   Updated: 2022/11/02 19:23:57 by fsariogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,38 @@ int	exec_main(t_commands *commands, int nb_pipes, char **envp)
 
 	i = 0;
 	if (!commands)
-		return ;
+		return (-1);
 	while (nb_pipes > 0)
 	{
 		if (nb_pipes > 1)
 			if (pipe(fd) == -1)
 				exit(EXIT_FAILURE);
+		printf("%d\n", fd[0]);
+		printf("%d\n", fd[1]);
 		pid = fork();
 		if (pid == 0)
 		{
-			printf("\ntesttt\n");
+			if (nb_pipes > 1)
+				dup2(fd[1], 1);
+			printf("Fils = %d\n", nb_pipes);
+			close(fd[0]);
+			close(fd[1]);
 			execve(commands[i].single_command[0], commands[i].single_command, envp);
 			exit(EXIT_SUCCESS);
 		}
 		else
 		{
+			printf("Parent = %d\n", nb_pipes);
 			waitpid(pid, &ret, 0);
 			if (ret != 0)
 				exit(EXIT_FAILURE);
-			dup2(1, 0);
+			close(fd[0]);
+			close(fd[1]);
+			if (nb_pipes > 1)
+				dup2(fd[0], 0);
 		}
 		nb_pipes--;
 		i++;
 	}
+	return (0);
 }
