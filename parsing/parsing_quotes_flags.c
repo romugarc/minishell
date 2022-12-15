b2c7fc4f304159ad257be6c0b5e0507b93e7c0a9
Romuald Garcia <rgarcia@z2r12p2.42lyon.fr>
2022-12-15 20:29:55 +0100

    fix definitif des quotes, allez ca passe

--- parsing/parsing_quotes_flags.c
+++ parsing/parsing_quotes_flags.c
@@ -6,83 +6,55 @@
 /*   By: fsariogl <fsariogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 15:49:32 by rgarcia           #+#    #+#             */
-/*   Updated: 2022/12/14 14:54:58 by rgarcia          ###   ########lyon.fr   */
+/*   Updated: 2022/12/15 18:18:30 by rgarcia          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static char	set_quote_flag(char *str, int *i, int *flag)
+static void	single_quotes_flags(char *line, char **quote_string, int *i)
 {
-	if (str[*i] == '\0')
-		return ('\0');
-	if (str[*i] == '"' && *flag == 0)
+	(*quote_string)[*i] = '0';
+	*i += 1;
+	while (line[*i] != 39 && line[*i] != '\0')
 	{
-		*flag = 2;
-		*i = *i + 1;
+		(*quote_string)[*i] = '1';
+		*i += 1;
 	}
-	else if (str[*i] == '"' && *flag == 2)
-	{
-		*flag = 0;
-		*i = *i + 1;
-	}
-	else if (str[*i] == 39 && *flag == 0)
-	{
-		*flag = 1;
-		*i = *i + 1;
-	}
-	else if (str[*i] == 39 && *flag == 1)
-	{
-		*flag = 0;
-		*i = *i + 1;
-	}
-	return ('0');
+	if (line[*i] == 39)
+		(*quote_string)[*i] = '0';
 }
 
-static char	what_quote(char *str, int i, int *flag)
+static void	double_quotes_flags(char *line, char **quote_string, int *i)
 {
-	if (str[i] != '\0')
+	(*quote_string)[*i] = '0';
+	*i += 1;
+	while (line[*i] != '"' && line[*i] != '\0')
 	{
-		if (*flag == 2 && str[i] != '"')
-			return ('2');
-		else if (*flag == 1 && str[i] != 39)
-			return ('1');
-		else if (*flag == 0)
-		{
-			if (i > 0)
-			{
-				if ((str[i - 1] == '"' || str[i - 1] == 39) && str[i] == 39)
-					*flag = 1;
-				if ((str[i - 1] == 39 || str[i - 1] == '"') && str[i] == '"')
-					*flag = 2;
-			}
-			return ('0');
-		}
+		(*quote_string)[*i] = '2';
+		*i += 1;
 	}
-	return ('\0');
+	if (line[*i] == '"')
+		(*quote_string)[*i] = '0';
 }
 
-int	quotes_flags(t_f_str *f_str, char *line)
+int quotes_flags(t_f_str *f_str, char *line)
 {
 	int		i;
-	int		j;
-	int		q_flag;
 	char	*quote_string;
 
-	i = 0;
-	q_flag = 0;
 	quote_string = malloc(sizeof(char) * ft_strlen(line) + 1);
 	if (!quote_string)
 		return (1);
+	i = 0;
 	while (line[i] != '\0')
 	{
-		j = i;
-		quote_string[i] = set_quote_flag(line, &j, &q_flag);
-		i = j;
-		if ((line[i] == '"' && q_flag != 1) || (line[i] == 39 && q_flag != 2))
-			quote_string[i] = set_quote_flag(line, &j, &q_flag);
-		i = j;
-		quote_string[i] = what_quote(line, i, &q_flag);
+		if (line[i] == '"')
+			double_quotes_flags(line, &quote_string, &i);
+		else if (line[i] == 39)
+			single_quotes_flags(line, &quote_string, &i);
+		else
+			quote_string[i] = '0';
 		if (line[i] != '\0')
 			i++;
 	}
